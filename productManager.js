3c30dc9d157af7e29c9b40c53bd8edd5dbc9fe95
filippettijuanm <filippetti_juanm@hotmail.com>
2023-06-05 22:17:15 -0300

    correccion1

--- productManager.js
+++ productManager.js
@@ -1,21 +1,14 @@
-// Definir la clase productManager
-
 class ProductManager{
     constructor() {
         this.products = [];
-        // Defino el constructor "products"
-        // Va a tener un arreglo vacio => para que el listado del producto me aparezca vacío
+        
 
     }
-//}
 
-// Debe contar con un metodo que retorne nuestro arreglo de productos
 getProducts = () => {
     return this.products;
 
 }
-
-// Para poder almacenar los productos en nuestro arreglo, debemos pasarle cuertos parametros
 
 addProduct = (title, description, price, thumbnail, code, stock) => {
     const product = {
@@ -25,7 +18,7 @@ addProduct = (title, description, price, thumbnail, code, stock) => {
         thumbnail,
         code,
         stock,
-        products: [] // Nos pedia el elemento "products" con array vacio
+        products: []
     }
 
     if (this.products.length === 0){
@@ -34,14 +27,11 @@ addProduct = (title, description, price, thumbnail, code, stock) => {
         product.id = this.products [this.products.length - 1].id+1
     }
 
-    // Pusheamos el producto
     this.products.push(product)
 }
 
-// Debe contar con el metodo getProductById, el cual recibe como parametro el Id del producto
-
 getProductById = (idProduct) =>{
-    cosnt productIndex = this.product.findIndex(product => product.id === idProduct);
+    const productIndex = this.products.findIndex(product => product.id === idProduct);
 
     if (productIndex === -1){
         console.log("Not found");
@@ -56,18 +46,18 @@ getProductById = (idProduct) =>{
     }
     this.products[productIndex].products.push(idProduct)
 }
-}
+};
 
 const menejadorProductos = new ProductManager ();
-manejadorProductos.addProduct ('Regla', 'Transparente', 80, 'Sin imagen', 'ab154', 36);
-manejadorProductos.addProduct ('Lapiz', 'Transparente', 100, 'Sin imagen', 'ab155', 30);
-manejadorProductos.addProduct ('Birome', 'Transparente', 200, 'Sin imagen', 'ab156', 15);
+menejadorProductos.addProduct ('Regla', 'Transparente', 80, 'Sin imagen', 'ab154', 36);
+menejadorProductos.addProduct ('Lapiz', 'Transparente', 100, 'Sin imagen', 'ab155', 30);
+menejadorProductos.addProduct ('Birome', 'Transparente', 200, 'Sin imagen', 'ab156', 15);
 
-manejadorProductos.getProductById(1);
-manejadorProductos.getProductById(2);
-manejadorProductos.getProductById(3);
+menejadorProductos.getProductById(1);
+menejadorProductos.getProductById(2);
+menejadorProductos.getProductById(3);
 
-console.log(manejadorProductos.getProducts());
+console.log(menejadorProductos.getProducts());
 
 
 
